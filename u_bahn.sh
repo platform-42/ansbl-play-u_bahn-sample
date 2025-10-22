2f873c8d9dd420368554e42f3236eff673cfcdea
Diederick de Buck <diederickdebuck@liberator.home>
2025-10-22 14:42:23 +0200

    rrrr

--- u_bahn.sh
+++ u_bahn.sh
@@ -3,5 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd ${SCRIPTPATH}
 
-ansible-galaxy collection install git@github.com:platform-42/platform42.neo4j.git,development
+ansible-galaxy collection install git@github.com:platform-42/platform42.neo4j.git
 ansible-playbook -i hosts/hosts.yml $(basename ${0%.*}).yml
